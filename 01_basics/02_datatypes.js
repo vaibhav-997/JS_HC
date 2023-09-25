9571a14662ade3b83985b7d4347eaa178b17e993
vaibhav-997 <143013678+vaibhav-997@users.noreply.github.com>
2023-09-25 15:19:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+'use strict'; //treat all JS code as newer version
+
+// alert('Hello')  //we are using node js not browser
+
+console.log(3
+    +3 );     // code readability should be high  -- should not write the non readable code 
+console.log(3 + 3);  
+
+
+
+// Data Types
+
+let name = 'Vaibhav'  // string
+let age = 20 //Number
+let isLoggeIn = false  //Boolean
+let state;  // undefined --- variable is asigned but value is not assigned
+
+// Primitive Data Types
+// number => 2
+// bigint
+// string => ""
+// boolean => true/false
+// null => Standalone value  -- empty
+// undefined => 
+// symbol => unique 
+
+// Objects
+
+console.log(typeof null); // null is a type of object
+console.log(typeof null); // undefined is a type
+
