d5a9eb7e426fb1c8ac8e7aa98f32c708e851d830
vaibhav-997 <143013678+vaibhav-997@users.noreply.github.com>
2023-09-26 10:12:14 +0000

    some extra points in ifee

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -9,10 +9,14 @@
 
 // iffe is used to remove the global scope pollution of the function to remove it IIFE is used
 (function chai(){
+    // named iife
     console.log('db connected')
-})()
-
-
+})();
+// if ; is not given it will not stop to stop the envoked function ; is necessary
+( (name)=> {
+    // un-named ifee
+console.log(`db connected two ${name}`)
+})('vaibhav');
 
 
 
