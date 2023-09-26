56f134108961a14d54d6a3f8a3e84ac1d9407a24
vaibhav-997 <143013678+vaibhav-997@users.noreply.github.com>
2023-09-26 12:07:57 +0000

    loop

---
+++ 05_Iterations/one.js
@@ -0,0 +1,70 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i
+//     if (element == 5){
+//         console.log('5 is a best number')
+//     }
+//     console.log(element);
+    
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value : ${i}`)
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`inner loop value : ${j} and outer loop ${i}`)
+//        console.log(i + '*' + j + ' = ' + i*j)
+        
+//     }
+    
+// }
+
+
+
+// const myArray = ['flash', 'batman', 'superman']
+
+// for(let i = 0; i < myArray.length; i ++){
+//     const element = myArray[i]
+//     console.log(element)
+// }
+
+
+
+
+
+// break ----- breaks the loop and not execute it further
+// continue --- continues for the value i.e skip the conditional value and execute further
+
+
+// while loop
+
+// let i = 0
+// while (i < 5){
+//     console.log(i)
+//     i++
+// }
+
+
+
+// do while loop
+
+// let i = 0
+
+// do{
+//     console.log(i)
+//     i++
+// }while(i < 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
